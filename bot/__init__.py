3a5c983bf649ee6ce1acfcb17ec7e61fb6bc3acd
prono69 <faltu9851@gmail.com>
2022-03-14 11:09:22 +0530

    os

--- bot/__init__.py
+++ bot/__init__.py
@@ -17,7 +17,7 @@ class ENV_VARS(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     LANGUAGE = os.environ.get("LANGUAGE", "EN")
     AUTH_USER = tuple(
-        filter(lambda x: x, map(int, environ.get("AUTH_USER", "0").split()))
+        filter(lambda x: x, map(int, os.environ.get("AUTH_USER", "0").split()))
     )
     MAX_MESSAGE_LENGTH = int(os.environ.get("MAX_MESSAGE_LENGTH", 4096))
 
